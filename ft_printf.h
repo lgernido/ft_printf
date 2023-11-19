46c19577ffafd9fed24334aa98248a05933bbe8b
Lucie Gernidos <lgernido@bess-f3r2s10.clusters.42paris.fr>
2023-11-19 13:55:21 +0100

    ft_printf

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 13:12:07 by lgernido          #+#    #+#             */
-/*   Updated: 2023/11/19 13:13:43 by lgernido         ###   ########.fr       */
+/*   Updated: 2023/11/19 13:52:20 by lgernido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,10 @@
 
 # include "libft.h"
 # include <stdarg.h>
+
+int	ft_printc(int c);
+int	ft_printstr(char *str);
+int	ft_printnb(int n);
+int	ft_printp(void);
 
 #endif
